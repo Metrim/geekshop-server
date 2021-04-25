ce28bf99020184b3576cc51aefa631b36db4a425
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-04-26 00:17:04 +0300

    Created "base.html". Added interactive work with context on Index and Products pages

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,8 +5,23 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590.00},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890.00},
+        ]
+    }
+    return render(request, 'mainapp/products.html', context)
+
