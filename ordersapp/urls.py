aa40154e9e01d4c63d483bed6c4a3c3421cf309c
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-06-27 23:57:53 +0300

    created webpage with view of the orders

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -5,10 +5,10 @@ from ordersapp.views import OrderList, OrderCreate, OrderRead, OrderUpdate, Orde
 app_name = 'ordersapp'
 
 urlpatterns = [
-    path('', OrderList.as_view, name='list'),
-    path('create/', OrderCreate.as_view, name='create'),
-    path('read/<int:pk>/', OrderRead.as_view, name='delete'),
-    path('update/<int:pk>/', OrderUpdate.as_view, name='update'),
-    path('delete/<int:pk>/', OrderDelete.as_view, name='delete'),
+    path('', OrderList.as_view(), name='list'),
+    path('create/', OrderCreate.as_view(), name='create'),
+    path('read/<int:pk>/', OrderRead.as_view(), name='read'),
+    path('update/<int:pk>/', OrderUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
     path('forming/complete/<int:pk>', forming_complete, name='forming_complete'),
 ]
