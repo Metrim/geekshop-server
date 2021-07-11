2cdfbea2ae48ce5e3e36c1e807a78ec7ded9a48c
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-07-11 14:34:00 +0300

    Reworked script to work with order - more stable

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -5,6 +5,7 @@ window.onload = function () {
     let price_arr = [];
 
     let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val());
+    let initial_total_forms = total_forms
 
     let order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
     let order_total_price = parseFloat($('.order_total_cost').text()) || 0;
@@ -14,10 +15,17 @@ window.onload = function () {
         total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
         quantity_arr = [];
         price_arr = [];
+        console.log('total_forms ', total_forms)
         for (let i = 0; i < total_forms; i++) {
             _quantity = parseInt($('input[name=orderitems-' + i + '-quantity]:visible').val())
             _price = parseFloat($('.orderitems-' + i + '-price:visible').text().replace(',', '.'))
             quantity_arr[i] = _quantity;
+            console.log('_quantity ', _quantity)
+            if (_quantity) {
+                quantity_arr[i] = _quantity;
+            } else {
+                quantity_arr[i] = 0;
+            }
             if (_price) {
                 price_arr[i] = _price;
             } else {
@@ -33,46 +41,47 @@ window.onload = function () {
     updateArrayPricesQuantity()
 
     $('.order_form').on('click', 'input[type=number]', function () {
-        let target = event.target; //  Выбираем на какой объект щелкнул пользователь
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', '')); // выбираем на какой строке пользователь
-        if (price_arr[orderitem_num]) {
-            orderitem_quantity = parseInt(target.value);
-            delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
-            quantity_arr[orderitem_num] = orderitem_quantity
+        // Below example worked with orderSummaryUpdate(orderitem_price, delta_quantity):
+        // let target = event.target; //  Выбираем на какой объект щелкнул пользователь
+        // orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', '')); // выбираем на какой строке пользователь
+        // if (price_arr[orderitem_num]) {
+        //     orderitem_quantity = parseInt(target.value);
+        //     delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+        //     quantity_arr[orderitem_num] = orderitem_quantity
+        // }
 
-            orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
-
-        }
+        orderSummaryRecalc();
     });
 
 
     $('.order_form').on('click', 'input[type=checkbox]', function () {
-        let target = event.target; //  Выбираем на какой объект щелкнул пользователь
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-        if (target.checked) {
-            delta_quantity = -quantity_arr[orderitem_num];
-        } else {
-            delta_quantity = quantity_arr[orderitem_num];
-        }
+        // Below example worked with orderSummaryUpdate(orderitem_price, delta_quantity):
+        // let target = event.target; //  Выбираем на какой объект щелкнул пользователь
+        // orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+        // if (target.checked) {
+        //     delta_quantity = -quantity_arr[orderitem_num];
+        // } else {
+        //     delta_quantity = quantity_arr[orderitem_num];
+        // }
 
-        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
-
+        orderSummaryRecalc();
     });
 
 
-    function orderSummaryUpdate(orderitem_price, delta_quantity) {
-        delta_cost = orderitem_price * delta_quantity;
-        console.log('delta_cost '+delta_cost)
-        order_total_price = Number((order_total_price + delta_cost).toFixed(2));
-        console.log('order_total_price '+order_total_price)
-        order_total_quantity = order_total_quantity + delta_quantity;
-        console.log('order_total_quantity '+order_total_quantity)
-
-
-        $('.order_total_quantity').text(order_total_quantity.toString());
-        $('.order_total_cost').text(order_total_price.toString());
-    }
-
+    // Changed this function implementation to the orderSummaryRecalc(); This entry only for history as example from lesson:
+    // function orderSummaryUpdate(orderitem_price, delta_quantity) {
+    //     delta_cost = orderitem_price * delta_quantity;
+    //     console.log('delta_cost '+delta_cost)
+    //     order_total_price = Number((order_total_price + delta_cost).toFixed(2));
+    //     console.log('order_total_price '+order_total_price)
+    //     order_total_quantity = order_total_quantity + delta_quantity;
+    //     console.log('order_total_quantity '+order_total_quantity)
+    //
+    //
+    //     $('.order_total_quantity').text(order_total_quantity.toString());
+    //     $('.order_total_cost').text(order_total_price.toString());
+    // }
+    //
 
     $('.formset_row').formset({
         addText: 'Добавить продукт',
@@ -83,65 +92,69 @@ window.onload = function () {
     });
 
     function deleteOrderItem(row) {
-        let target_name = row[0].querySelector('input[type=number]').name;
-        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
-
-
-        delta_quantity = -quantity_arr[orderitem_num];
-        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
-        updateArrayPricesQuantity()
+        orderSummaryRecalc();
+        // Changed this function implementation to the orderSummaryRecalc(); This entry only for history as example from lesson:
+        // let target_name = row[0].querySelector('input[type=number]').name;
+        // orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+        // delta_quantity = -quantity_arr[orderitem_num];
+        // orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
+        // updateArrayPricesQuantity()
     }
 
     function addOrderItem() {
-        updateArrayPricesQuantity()
+        updateArrayPricesQuantity();
+        let current_tr = $('.order_form table').find('tr:eq(' + total_forms + ')');
+        current_tr.find('input[type="number"]').val(0);
+        updateSelectChangeHandler();
     }
-    
-    $('.order_form select').change(function () {
-        let target = event.target;
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
 
-        let order_item_product_pk = target.options[target.selectedIndex].value;
-        console.log('order_item_product_pk ' + order_item_product_pk)
-        if (order_item_product_pk == '') {
-            order_item_product_pk = 0
+    // Window onload call of the function:
+    updateSelectChangeHandler()
+
+    function updateSelectChangeHandler() {
+        $('.order_form select').change(function () {
+            let target = event.target;
+            orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+            console.log('orderitem_num', orderitem_num)
+            let order_item_product_pk = target.options[target.selectedIndex].value;
             console.log('order_item_product_pk ' + order_item_product_pk)
-        }
+            if (order_item_product_pk === '') {
+                order_item_product_pk = 0
+                console.log('order_item_product_pk ' + order_item_product_pk)
+            }
 
-        if (order_item_product_pk || order_item_product_pk === 0) {
-            $.ajax({
-                url: '/order/product/' + order_item_product_pk + '/price/',
-                success: function (data) {
-                    if (data.price || data.price === 0) {
-                        console.log('success')
-                        console.log(data.price)
-                        price_arr[orderitem_num] = parseFloat(data.price);
-                        if (isNaN(quantity_arr[orderitem_num])) {
-                            quantity_arr[orderitem_num] = 0;
+            if (order_item_product_pk || order_item_product_pk === 0) {
+                $.ajax({
+                    url: '/order/product/' + order_item_product_pk + '/price/',
+                    success: function (data) {
+                        if (data.price || data.price === 0) {
+                            console.log('success data.price', data.price)
+                            price_arr[orderitem_num] = parseFloat(data.price);
+                            if (isNaN(quantity_arr[orderitem_num])) {
+                                quantity_arr[orderitem_num] = 0;
+                            }
+                            let price_html ='<span class="orderitems-' + orderitem_num + '-price">' + data.price.toString().replace(".", ",") + '&nbsp;руб</span>'
+                            let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                            current_tr.find('td:eq(2)').html(price_html);
+                            if (data.price === 0) {
+                                current_tr.find('input[type="number"]').val(0);
+                            }
+                            orderSummaryRecalc();
                         }
-                        let price_html ='<span class="orderitems-' + orderitem_num + '-price">' + data.price.toString().replace(".", ",") + '&nbsp;руб</span>'
-                        let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
-                        current_tr.find('td:eq(2)').html(price_html);
-                        if (data.price === 0) {
-                            current_tr.find('input[type="number"]').val(0);
-                        }
-                        orderSummaryRecalc();
                     }
-                }
-            });
-        }
-
-    });
-
+                });
+            }
+        });
+    }
 
     function orderSummaryRecalc() {
         order_total_quantity = 0;
         order_total_price = 0;
         console.log('ordersummaryrecalc')
+        updateArrayPricesQuantity()
         for (let i = 0; i < total_forms; i++) {
             order_total_quantity += quantity_arr[i];
-            order_total_price += price_arr[i];
-            console.log('order_total_quantity ' + order_total_quantity)
-            console.log('order_total_price ' + order_total_price)
+            order_total_price += quantity_arr[i] * price_arr[i];
         }
 
         $('.order_total_quantity').html(order_total_quantity.toString());
