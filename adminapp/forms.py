947f574097d7aace989c9226529c0ab6de0d9fbd
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-07-22 21:55:36 +0300

    created F-object for discount application in admin tab

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.db import connection
+from django.db.models import F
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
 
 from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
+from mainapp.models import ProductCategory
 
 
 class UserAdminRegisterForm(UserRegisterForm):
@@ -23,3 +28,45 @@ class UserAdminProfileForm(UserProfileForm):
         'class': 'form-control py-4',
         'readonly': False
     }))
+
+
+# For the lesson 8 product category discount should be added into the custom admin page for discount
+class ProductCategoryEditForm(forms.ModelForm):
+    discount = forms.IntegerField(label='скидка', required=False, min_value=0, max_value=90, initial=0)
+
+    class Meta:
+        model = ProductCategory
+        # fields = '__all__'
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(ProductCategoryEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widjet.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+def db_profile_by_type(__class__, param, queries):
+    pass
+
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    form_class = ProductCategoryEditForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+        return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set. \
+                    update(price=F('price') * (1 - discount / 100))
+                db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
+
+        return super().form_valid(form)
